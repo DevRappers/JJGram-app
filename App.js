5deac713c861ea591dbd07e2e005e8bdd09b47a8
DevRappers <devrappers@gmail.com>
2019-12-02 17:37:35 +0900

    Preloading Cache ( 이전에 사용했던 정보를 저장하고 시작할때 정보 불러오기 )

--- App.js
+++ App.js
@@ -3,10 +3,17 @@ import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage } from 'react-native';
+import { InMomoryCache, InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo-hooks';
+import apolloClientOptions from './apollo';
 
 export default function App() {
+	// 로딩관련과 client관련
 	const [ loaded, setLoaded ] = useState(false);
+	const [ client, setClient ] = useState(null);
 
 	const preLoad = async () => {
 		try {
@@ -14,7 +21,20 @@ export default function App() {
 				...Ionicons.font
 			});
 			await Asset.loadAsync([ require('./assets/logo.png') ]);
+
+			// cache로 이전에 사용했던 정보 저장해놓고 불러올때 사용하는 부분
+			const cache = new InMemoryCache();
+			await persistCache({
+				cache,
+				storage: AsyncStorage
+			});
+			const client = new ApolloClient({
+				cache,
+				...apolloClientOptions
+			});
+
 			setLoaded(true);
+			setClient(client);
 		} catch (e) {
 			console.log(e);
 		}
@@ -24,10 +44,12 @@ export default function App() {
 		preLoad();
 	}, []);
 
-	return loaded ? (
-		<View>
-			<Text>Hello</Text>
-		</View>
+	return loaded && client ? (
+		<ApolloProvider client={client}>
+			<View>
+				<Text>Hello</Text>
+			</View>
+		</ApolloProvider>
 	) : (
 		<AppLoading />
 	);
