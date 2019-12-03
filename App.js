cfad3ed01043e282f8b27ea8367e063e1123445c
DevRappers <devrappers@gmail.com>
2019-12-03 09:39:04 +0900

    isLoggedIn ( 로그인 관련 AsyncStorage 생성해서 state관리하기 )

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
-import { Text, View, AsyncStorage } from 'react-native';
-import { InMomoryCache, InMemoryCache } from 'apollo-cache-inmemory';
+import * as Font from 'expo-font';
+import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
+import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from 'react-apollo-hooks';
 import apolloClientOptions from './apollo';
+import styles from './styles';
 
 export default function App() {
-	// 로딩관련과 client관련
+	// 로딩관련, client관련, 로그인관련 state
 	const [ loaded, setLoaded ] = useState(false);
 	const [ client, setClient ] = useState(null);
+	const [ isLoggedIn, setIsLoggedIn ] = useState(null);
 
+	// AsyncStorage에서 여러 정보를 가져오는 함수
 	const preLoad = async () => {
 		try {
+			// 폰트 불러오기
 			await Font.loadAsync({
 				...Ionicons.font
 			});
-			await Asset.loadAsync([ require('./assets/logo.png') ]);
 
-			// cache로 이전에 사용했던 정보 저장해놓고 불러올때 사용하는 부분
+			// 앱에 필요한 이미지를 불러옴
+			await Asset.loadAsync([ require('./assets/logo.png') ]);
 			const cache = new InMemoryCache();
 			await persistCache({
 				cache,
 				storage: AsyncStorage
 			});
+
+			// 클라이언트 생성
 			const client = new ApolloClient({
 				cache,
 				...apolloClientOptions
 			});
+
+			// 이전에 로그인되었었는지 상태를 확인함
+			const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+			// AsyncStorage에 string형태로 저장되기 때문에 string으로 비교해줌
+			if (isLoggedIn === null || isLoggedIn === 'false') {
+				setIsLoggedIn(false);
+			} else {
+				setIsLoggedIn(true);
+			}
 
 			setLoaded(true);
 			setClient(client);
@@ -40,15 +56,47 @@ export default function App() {
 		}
 	};
 
+	// useEffect로 앱 시작시 preLoad함수를 호출함
 	useEffect(() => {
 		preLoad();
 	}, []);
 
-	return loaded && client ? (
+	// 로그인 함수
+	const logUserIn = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'true');
+			setIsLoggedIn(true);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	// 로그아웃 함수
+	const logUserOut = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'false');
+			setIsLoggedIn(false);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	// 로딩이 완료되고 client가 있으며 로그인상태가 null이 아닐경우 화면을 보여주고 그렇지 않을 경우 로딩창 출력
+	return loaded && client && isLoggedIn !== null ? (
 		<ApolloProvider client={client}>
-			<View>
-				<Text>Hello</Text>
-			</View>
+			<ThemeProvider theme={styles}>
+				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					{isLoggedIn === true ? (
+						<TouchableOpacity onPress={logUserOut}>
+							<Text>Log Out</Text>
+						</TouchableOpacity>
+					) : (
+						<TouchableOpacity onPress={logUserIn}>
+							<Text>Log In</Text>
+						</TouchableOpacity>
+					)}
+				</View>
+			</ThemeProvider>
 		</ApolloProvider>
 	) : (
 		<AppLoading />
