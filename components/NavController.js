0cede380c271e13c3ed4da659160d0ebaaf92bcc
DevRappers <devrappers@gmail.com>
2019-12-03 11:19:13 +0900

    AuthNavigation만들기

--- components/NavController.js
+++ components/NavController.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { useIsLoggedIn, useLogIn, useLogOut } from '../AuthContext';
+import AuthNavigation from '../navigation/AuthNavigation';
 
 export default () => {
-	// 컨텍스트함수를 사용함
+	// 컨텍스트함수를 사용
 	const isLoggedIn = useIsLoggedIn();
 	const logIn = useLogIn();
 	const logOut = useLogOut();
 
 	return (
-		<View style={{ flex: '1', justifyContent: 'center', alignItems: 'center' }}>
+		<View style={{ flex: '1' }}>
 			{isLoggedIn ? (
 				<TouchableOpacity onPress={logOut}>
 					<Text>Log Out</Text>
 				</TouchableOpacity>
 			) : (
-				<TouchableOpacity onPress={logIn}>
-					<Text>Log In</Text>
-				</TouchableOpacity>
+				<AuthNavigation />
 			)}
 		</View>
 	);
