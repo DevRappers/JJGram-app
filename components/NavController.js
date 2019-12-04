ecd42ebe73d8ebce5d05dde737211d350bd7fedd
DevRappers <devrappers@gmail.com>
2019-12-04 15:19:26 +0900

    AuthHome 만들기 초기화면 (로그인과 회원가입 중 선택할 수 있도록 )

--- components/NavController.js
+++ components/NavController.js
@@ -6,6 +6,6 @@ import MainNavigation from '../navigation/MainNavigation';
 
 export default () => {
 	// 컨텍스트함수를 사용
-	const isLoggedIn = true;
+	const isLoggedIn = false;
 	return <View style={{ flex: '1' }}>{isLoggedIn ? <MainNavigation /> : <AuthNavigation />}</View>;
 };
