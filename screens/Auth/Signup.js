f10a61dda4a0fda6919524fcf28e9363486cc62e
DevRappers <devrappers@gmail.com>
2019-12-05 10:22:03 +0900

    Facebook login, google login 가능하게 기능 구현

--- screens/Auth/Signup.js
+++ screens/Auth/Signup.js
@@ -7,11 +7,21 @@ import useInput from '../../hooks/useInput';
 import { Alert } from 'react-native';
 import { useMutation } from 'react-apollo-hooks';
 import { CREATE_ACCOUNT } from './AuthQueries';
+import * as Facebook from 'expo-facebook';
+import * as Google from 'expo-google-app-auth';
 
 const View = styled.View`
 	justify-content: center;
 	align-items: center;
 	flex: 1;
+`;
+
+const FBContainer = styled.View`
+	margin-top: 25px;
+	padding-top: 25px;
+	border-top-width: 1px;
+	border-top-color: ${(props) => props.theme.lightGreyColor};
+	border-style: solid;
 `;
 
 export default ({ navigation }) => {
@@ -58,6 +68,61 @@ export default ({ navigation }) => {
 			setLoading(false);
 		}
 	};
+	const fbLogin = async () => {
+		try {
+			setLoading(true);
+			const { type, token } = await Facebook.logInWithReadPermissionsAsync('2885307264822472', {
+				permissions: [ 'public_profile', 'email' ]
+			});
+			if (type === 'success') {
+				// Get the user's name using Facebook's Graph API
+				const response = await fetch(
+					`https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`
+				);
+				const { email, first_name, last_name } = await response.json();
+				updateFormData(email, first_name, last_name);
+				Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+				setLoading(false);
+			} else {
+				// type === 'cancel'
+			}
+		} catch ({ message }) {
+			alert(`Facebook Login Error: ${message}`);
+			setLoading(false);
+		}
+	};
+	const googleLogin = async () => {
+		const GOOGLE_ID = '160434121506-33fm950jr4ihgpht18r1ccr6rfet9nrd.apps.googleusercontent.com';
+		try {
+			setLoading(true);
+			const result = await Google.logInAsync({
+				iosClientId: GOOGLE_ID,
+				scopes: [ 'profile', 'email' ]
+			});
+			if (result.type === 'success') {
+				const user = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+					headers: { Authorization: `Bearer ${result.accessToken}` }
+				});
+				const { email, family_name, given_name } = await user.json();
+				updateFormData(email, family_name, given_name);
+				setLoading(false);
+			} else {
+				return { cancelled: true };
+			}
+		} catch (e) {
+			setLoading(false);
+			return { error: true };
+		} finally {
+			setLoading(false);
+		}
+	};
+	const updateFormData = (email, firstName, lastName) => {
+		emailInput.setValue(email);
+		fNameInput.setValue(firstName);
+		lNameInput.setValue(lastName);
+		const [ username ] = email.split('@');
+		nameInput.setValue(username);
+	};
 	return (
 		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 			<View>
@@ -72,6 +137,10 @@ export default ({ navigation }) => {
 				/>
 				<AuthInput {...nameInput} placeholder="사용자이름" returnKeyType="send" autoCorrect={false} />
 				<AuthButton loading={loading} onPress={handleSingup} text="회원가입" />
+				<FBContainer>
+					<AuthButton bgColor={'#2D4DA7'} loading={false} onPress={fbLogin} text="Facebook 연결" />
+					<AuthButton bgColor={'#EE1922'} loading={false} onPress={googleLogin} text="Google 연결" />
+				</FBContainer>
 			</View>
 		</TouchableWithoutFeedback>
 	);
