d7dd637936700d9a3b56a774146c0e8eaaf7c98e
DevRappers <devrappers@gmail.com>
2019-12-06 10:50:19 +0900

    Post Conponent Header	만들기

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Loader from '../../components/Loader';
 import { gql } from 'apollo-boost';
 import { useQuery } from 'react-apollo-hooks';
+import Post from '../../components/Post';
 
 const FEED_QUERY = gql`
 	{
@@ -58,7 +59,11 @@ export default () => {
 	};
 	return (
 		<ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}>
-			{loading ? <Loader /> : <Text>Hello</Text>}
+			{loading ? (
+				<Loader />
+			) : (
+				data && data.seeFeed && data.seeFeed.map((post) => <Post key={post.id} {...post} />)
+			)}
 		</ScrollView>
 	);
 };
